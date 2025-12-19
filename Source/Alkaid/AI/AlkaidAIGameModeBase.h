3734c93de7db2ff35d747603bc783aeb7a2bcb60
soo-10 <uoosoo0723@gmail.com>
2025-12-19 21:00:02 +0900

    add Class

---
+++ Source/Alkaid/AI/AlkaidAIGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AlkaidAIGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALKAID_API AAlkaidAIGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
