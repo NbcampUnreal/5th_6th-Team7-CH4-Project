19ca215c2efc6dd40fcd471be9075ac38295453e
유용선(Unreal 6기) <dtoli0930@gmail.com>
2025-12-23 12:37:44 +0900

    [fix] fixed build.cs
    build.cs NaiagaraSystem

--- Source/Alkaid/Alkaid.Build.cs
+++ Source/Alkaid/Alkaid.Build.cs
@@ -14,7 +14,7 @@ public class Alkaid : ModuleRules
 
         PublicIncludePaths.AddRange(new string[]
         {
-            "Alkaid","Server","NavigationSystem", "Niagara",
+            "Alkaid","Server",
         });
 
         // Uncomment if you are using Slate UI
