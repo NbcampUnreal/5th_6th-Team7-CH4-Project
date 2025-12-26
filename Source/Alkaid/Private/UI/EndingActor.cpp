66aeb9466091613705e5f4098b90956cf1b5506c
Give291 <whdydglgk@gmail.com>
2025-12-26 17:09:21 +0900

    add 12/26

---
+++ Source/Alkaid/Private/UI/EndingActor.cpp
@@ -0,0 +1,22 @@
+#include "UI/EndingActor.h"
+
+AEndingActor::AEndingActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+
+void AEndingActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+void AEndingActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/Alkaid/Public/UI/EndingActor.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EndingActor.generated.h"
+
+class UBoxComponent;
+class UWidgetComponent;
+
+UCLASS()
+class ALKAID_API AEndingActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	
+	AEndingActor();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "EndingMesh")
+	UStaticMeshComponent* EndingMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "EndingBoxComponent")
+	UBoxComponent* EndingBoxCollision;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "UIComponent")
+	TObjectPtr<UWidgetComponent> EndingWidgetComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "TextComponent")
+	TObjectPtr<UWidgetComponent> EndingTextWidgetComponent;
+
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+	//UFUNCTION(BlueprintCallable, Category = "EndingAnim")
+	//void PlayEndingWidgetAnim();
+
+};
