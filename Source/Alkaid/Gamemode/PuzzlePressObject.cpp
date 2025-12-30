7e58da2f0963e7cb79a60ad773121093fa88d41d
유용선(Unreal 6기) <dtoli0930@gmail.com>
2025-12-30 21:59:40 +0900

    [fix] fixed Replicated Puzzle Pressed
    fixed character attach

--- Source/Alkaid/Gamemode/PuzzlePressObject.cpp
+++ Source/Alkaid/Gamemode/PuzzlePressObject.cpp
@@ -1,12 +1,20 @@
 #include "Gamemode/PuzzlePressObject.h"
 #include "Components/StaticMeshComponent.h"
+#include "Net/UnrealNetwork.h"
 
 APuzzlePressObject::APuzzlePressObject()
 {
     PrimaryActorTick.bCanEverTick = false;
+    SetReplicateMovement(true);
+    bReplicates = true;
+    NetUpdateFrequency = 60.f;
+    MinNetUpdateFrequency = 30.f;
 
     Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
     SetRootComponent(Mesh);
+
+    Mesh->SetIsReplicated(true);
+    Mesh->SetGenerateOverlapEvents(true);
 
     Mesh->SetSimulatePhysics(true);
     Mesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
